208267c4fc44552388e3b4fb876e3a4b81d549b1
Radoslav Georgiev <rgeorgiev583@gmail.com>
2025-12-03 23:37:06 +0200

    add solutions for third day puzzle (both parts)

---
+++ 3-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  int total_joltage{0};
+
+  while (cin.good()) {
+    string line;
+    getline(cin, line);
+    if (cin.bad()) {
+      cerr << "error: could not read line from standard input" << endl;
+      return 1;
+    }
+    if (line.empty())
+      continue;
+
+    if (line.length() < 2) {
+      cerr << "error: line has less than two characters" << endl;
+      return 1;
+    }
+
+    int max_number{0};
+    size_t max_number_pos{string::npos};
+    for (size_t i{0}; i < line.length() - 1; i++) {
+      int number{line[i] - '0'};
+      if (number > max_number) {
+        max_number = number;
+        max_number_pos = i;
+      }
+    }
+
+    int second_max_number{0};
+    for (size_t i{max_number_pos + 1}; i < line.length(); i++) {
+      int number{line[i] - '0'};
+      if (number > second_max_number)
+        second_max_number = number;
+    }
+
+    total_joltage += max_number * 10 + second_max_number;
+  }
+
+  cout << total_joltage << endl;
+
+  return 0;
+}
---
+++ 3-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  long total_joltage{0};
+
+  while (cin.good()) {
+    string line;
+    getline(cin, line);
+    if (cin.bad()) {
+      cerr << "error: could not read line from standard input" << endl;
+      return 1;
+    }
+    if (line.empty())
+      continue;
+
+    if (line.length() < 12) {
+      cerr << "error: line has less than twelve characters" << endl;
+      return 1;
+    }
+
+    size_t max_number_pos{string::npos};
+    long multiplier{100000000000};
+    for (int i{11}; i >= 0; i--) {
+      int max_number{0};
+      for (size_t j{max_number_pos + 1}; j < line.length() - i; j++) {
+        int number{line[j] - '0'};
+        if (number > max_number) {
+          max_number = number;
+          max_number_pos = j;
+        }
+      }
+
+      total_joltage += max_number * multiplier;
+      multiplier /= 10;
+    }
+  }
+
+  cout << total_joltage << endl;
+
+  return 0;
+}
