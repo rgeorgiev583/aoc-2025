d0d12139fbba5b1ca4b8efd2b62f6c759c84d6f0
Radoslav Georgiev <rgeorgiev583@gmail.com>
2025-12-09 14:07:51 +0200

    add solution for first part of sixth day puzzle

---
+++ 6-1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main() {
+  using namespace std;
+
+  vector<vector<string>> problems;
+
+  while (cin.good()) {
+    string line;
+    getline(cin, line);
+    if (cin.bad()) {
+      cerr << "error: could not read line from standard input" << endl;
+      return 1;
+    }
+    if (line.empty())
+      continue;
+
+    vector<string> row;
+    if (!problems.empty())
+      row.reserve(problems[0].size());
+    istringstream line_stream{line};
+    while (line_stream.good()) {
+      string field;
+      line_stream >> field;
+      if (!field.empty())
+        row.push_back(field);
+    }
+
+    if (!problems.empty() && row.size() != problems[0].size()) {
+      cerr << "warning: row has a different number of fields" << endl;
+      return 1;
+    }
+
+    problems.push_back(std::move(row));
+  }
+
+  if (problems.empty()) {
+    cerr << "error: empty standard input" << endl;
+    return 1;
+  }
+
+  auto parse_number{[](const string &p_str) -> unsigned long long {
+    istringstream str_stream{p_str};
+    unsigned long long number;
+    str_stream >> number;
+    if (!str_stream) {
+      cerr << "error: could not parse number from field" << endl;
+      return 1;
+    }
+
+    return number;
+  }};
+  vector<unsigned long long> answers;
+  if (!problems.empty()) {
+    answers.reserve(problems[0].size());
+    for (const string &field : problems[0])
+      answers.push_back(parse_number(field));
+    const auto &operators{problems[problems.size() - 1]};
+    for (size_t i{1}; i < problems.size() - 1; i++) {
+      for (size_t j{0}; j < operators.size(); j++) {
+        unsigned long long field{parse_number(problems[i][j])};
+
+        if (operators[j] == "+")
+          answers[j] += field;
+        else if (operators[j] == "*")
+          answers[j] *= field;
+        else {
+          cerr << "error: invalid operator: " << operators[j] << endl;
+          return 1;
+        }
+      }
+    }
+  }
+
+  unsigned long long sum_answers{0};
+  for (const auto &answer : answers)
+    sum_answers += answer;
+
+  cout << sum_answers << endl;
+
+  return 0;
+}
